a5367073a29e718d12ce58bfbda470c904157fc9
varunwalia120 <varunwalia608@gmail.com>
2026-07-23 12:17:10 +0530

    feat: manage weather application states

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,37 @@ import { useState } from "react";
 import Navbar from "./components/layout/Navbar";
 import SearchBar from "./components/search/SearchBar";
 import WeatherCard from "./components/weather/WeatherCard";
+
+import EmptyState from "./components/common/EmptyState";
+import Loading from "./components/common/Loading";
+import ErrorMessage from "./components/common/ErrorMessage";
+
 import { getWeatherByCity } from "./services/weatherApi";
 import type { WeatherResponse } from "./types/weather";
-import EmptyState from "./components/common/EmptyState";
 
 function App() {
   const [weather, setWeather] = useState<WeatherResponse | null>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const handleSearch = async (city: string) => {
     try {
-      console.log("Searching:", city);
+      setLoading(true);
+      setError("");
 
       const data = await getWeatherByCity(city);
 
-      console.log(data);
-
       setWeather(data);
-    } catch (error) {
-      console.error(error);
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Something went wrong.");
+      }
+
+      setWeather(null);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -29,7 +42,15 @@ function App() {
 
       <SearchBar onSearch={handleSearch} />
 
-      {!weather ? <EmptyState /> : <WeatherCard weather={weather} />}
+      {loading ? (
+        <Loading />
+      ) : error ? (
+        <ErrorMessage message={error} />
+      ) : weather ? (
+        <WeatherCard weather={weather} />
+      ) : (
+        <EmptyState />
+      )}
     </main>
   );
 }
